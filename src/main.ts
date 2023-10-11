87290f8e74eb3d37f269507375cd7d475f6d3234
Jimmy Nguyen <nguyenjimmy627@gmail.com>
2023-10-10 20:46:01 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "The Catnip Market";
+const buttonName = "🐱";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = buttonName;
+app.append(button);
