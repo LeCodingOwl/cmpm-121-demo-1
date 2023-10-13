df214fb2e0ac79c13ccee026a5129af8d1536ff6
Jimmy Nguyen <nguyenjimmy627@gmail.com>
2023-10-13 12:51:28 -0700

    refactor repeating code

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "The Catnip Market";
-const buttonName = "🐾 YEAH TAP YOUR PAWS";
+const mainClickerName = "🐾 YEAH TAP YOUR PAWS";
+
+document.title = gameName;
 
 const priceIncrease = 1.15;
 
@@ -14,6 +16,8 @@ interface Item {
   name: string;
   cost: number;
   rate: number;
+  amount: number;
+  button: HTMLButtonElement;
   description: string;
 }
 
@@ -22,128 +26,130 @@ const availableItems: Item[] = [
     name: "Hire simple worker cat 🐱",
     cost: 10,
     rate: 0.1,
+    amount: 0,
+    button: document.createElement("button"),
     description: "Just a simple cat. He's doing his best!",
   },
   {
     name: "Hire Hipster cat 🐱‍👓",
     cost: 100,
     rate: 2,
+    amount: 0,
+    button: document.createElement("button"),
     description: "This cat is tech savvy. *Sips tea*",
   },
   {
     name: "Hire Ninja cat 🐱‍👤",
     cost: 1000,
     rate: 50,
+    amount: 0,
+    button: document.createElement("button"),
     description: "OH YEAH YOU SEE WHAT THESE PAWS CAN DO?",
   },
   {
     name: "Hire Hacker cat 🐱‍💻",
     cost: 10000,
     rate: 100,
+    amount: 0,
+    button: document.createElement("button"),
     description: "Diving deep into the dark cat web",
   },
   {
     name: "Hire Space Cat 🐱‍🚀",
     cost: 100000,
     rate: 10000,
+    amount: 0,
+    button: document.createElement("button"),
     description: "Harvesting Mars rocks!",
   },
 ];
 
+//Setting up game layout -------------------------
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const counterText = document.createElement("div");
-counterText.innerHTML = "Number of catnips produced: " + counter.toFixed();
+const button = document.createElement("button");
+button.innerHTML = mainClickerName;
+button.addEventListener("click", () => {
+  counter++;
+});
+app.append(button);
+
+const counterText: HTMLDivElement = document.createElement("div");
+counterText.innerHTML = "Number of catnips produced: " + counter.toFixed(2);
 app.append(counterText);
 
-const growthText = document.createElement("div");
+const growthText: HTMLDivElement = document.createElement("div");
 growthText.innerHTML =
-  "Current growth rate of Catnip: " + counterGrowth.toFixed(1) + "/sec";
+  "Current growth rate of Catnip: " + counterGrowth.toFixed(2) + "/sec";
 app.append(growthText);
 
-// Upgrade buttons
-const upgradeButton = document.createElement("button");
-app.append(upgradeButton);
+createButtons(availableItems);
 
-upgradeButton.addEventListener("click", () => {
-  counterGrowth += availableItems[0].rate;
-  counter -= availableItems[0].cost;
-  availableItems[0].cost += priceIncrease;
-});
+//Functions ------------------------------------
+function createButtons(buttons: Item[]) {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].button.innerHTML =
+      availableItems[i].name +
+      " (" +
+      availableItems[i].amount +
+      " cats hired) | " +
+      "Cost: " +
+      availableItems[i].cost.toFixed(2) +
+      " Catnips" +
+      " | " +
+      availableItems[i].rate.toFixed(2) +
+      "/sec" +
+      "<br><font size=-1>" +
+      availableItems[i].description;
+    buttons[i].button.disabled = true;
+    app.append(buttons[i].button);
 
-const upgradeButton2 = document.createElement("button");
-app.append(upgradeButton2);
-
-upgradeButton2.addEventListener("click", () => {
-  counterGrowth += availableItems[1].rate;
-  counter -= availableItems[1].cost;
-  availableItems[1].cost += priceIncrease;
-});
-
-const upgradeButton3 = document.createElement("button");
-app.append(upgradeButton3);
-
-upgradeButton3.addEventListener("click", () => {
-  counterGrowth += availableItems[2].rate;
-  counter -= availableItems[2].cost;
-  availableItems[2].cost += priceIncrease;
-});
-
-const upgradeButton4 = document.createElement("button");
-app.append(upgradeButton4);
-
-upgradeButton4.addEventListener("click", () => {
-  counterGrowth += availableItems[3].rate;
-  counter -= availableItems[3].cost;
-  availableItems[3].cost += priceIncrease;
-});
-
-const upgradeButton5 = document.createElement("button");
-app.append(upgradeButton5);
-
-upgradeButton5.addEventListener("click", () => {
-  counterGrowth += availableItems[4].rate;
-  counter -= availableItems[4].cost;
-  availableItems[4].cost += priceIncrease;
-});
-
-function checkCost() {
-  if (counter >= availableItems[0].cost) {
-    upgradeButton.disabled = false;
-  } else {
-    upgradeButton.disabled = true;
+    availableItems[i].button.addEventListener("click", () =>
+      purchaseItem(availableItems[i]),
+    );
   }
-  }
-  }
+}
 
-  if (counter >= availableItems[3].cost) {
-    upgradeButton4.disabled = false;
-  } else {
-    upgradeButton4.disabled = true;
-  }
+function purchaseItem(selectedItem: Item) {
+  counterGrowth += selectedItem.rate;
+  counter -= selectedItem.cost;
+  selectedItem.cost *= priceIncrease;
+  selectedItem.amount += 1;
+  selectedItem.button.innerHTML =
+    selectedItem.name +
+    " (" +
+    selectedItem.amount +
+    " cats hired) | " +
+    "Cost: " +
+    selectedItem.cost.toFixed(2) +
+    " Catnips" +
+    " | " +
+    selectedItem.rate.toFixed(2) +
+    "/sec" +
+    "<br><font size=-1>" +
+    selectedItem.description;
+}
 
-function checkCost (buttons: Item[])
-{
+function checkCost(buttons: Item[]) {
   availableItems.forEach((item, i) => {
     buttons[i].button.disabled = item.cost > counter;
-  })
+  });
 }
 
 let lastTimestamp: number = 0;
 function autoCounter(timestamp: number) {
   const timePassed = timestamp - lastTimestamp;
   lastTimestamp = timestamp;
-  requestAnimationFrame(autoCounter);
+
+  counterText.innerHTML = "Number of catnips produced: " + counter.toFixed(2);
+  growthText.innerHTML =
+    "Current growth rate of Catnip: " + counterGrowth.toFixed(2) + "/sec";
 
   counter += counterGrowth * (timePassed / 1000);
-  checkCost(availableItems);
 
-  counterText.innerHTML = "Number of catnips produced: " + counter.toFixed();
-  growthText.innerHTML = "Current growth rate of Catnip: " + counterGrowth.toFixed(1) + "/sec";
+  checkCost(availableItems);
+  requestAnimationFrame(autoCounter);
 }
-requestAnimationFrame(autoCounter);  checkCost();
-  updateText();
-}
-autoCounter();
+requestAnimationFrame(autoCounter);
